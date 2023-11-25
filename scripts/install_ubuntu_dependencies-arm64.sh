93452da3d0fefdaa94467cfd50bbd9fd6208d349
C.Lee Taylor <leet@tari.com>
2023-11-25 13:15:55 +0200

    ci(fix): move arm64 ubuntu hackery into script

--- scripts/install_ubuntu_dependencies-arm64.sh
+++ scripts/install_ubuntu_dependencies-arm64.sh
@@ -7,3 +7,17 @@ apt-get -y install $* \
   g++-aarch64-linux-gnu
 
 #  pkg-config-aarch64-linux-gnu \
+
+# Big hack to get libssl-dev:arm64 on ubuntu 22.04 - https://askubuntu.com/questions/1255707/apt-cant-find-packages-on-ubuntu-20-04-arm64-raspberry-pi-4
+cat << EOD | tee -a /etc/apt/sources.list.d/ubuntu-ports-arm64.list
+deb [arch=arm64] http://ports.ubuntu.com/ jammy main multiverse universe
+deb [arch=arm64] http://ports.ubuntu.com/ jammy-security main multiverse universe
+deb [arch=arm64] http://ports.ubuntu.com/ jammy-backports main multiverse universe
+deb [arch=arm64] http://ports.ubuntu.com/ jammy-updates main multiverse universe
+EOD
+dpkg --print-foreign-architectures
+dpkg --add-architecture arm64
+dpkg --print-foreign-architectures
+# Not all repos have arm64 components, force true return
+apt-get update || true
+apt-get install -y libssl-dev:arm64
