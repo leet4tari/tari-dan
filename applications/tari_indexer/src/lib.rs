leet4tari/tari-dan

commit f45c0c1bc5cc89adb481058b8dc840065ad62f61

fix: remove epoch manager calls from consensus gossipsub (#1190)
Description
---
fix: remove epoch manager calls from consensus gossipsub

Motivation and Context
---
Several epoch manager calls were used for message broadcasts, however,
all required info obtained from these calls is already available at the
consensus level, this PR removes the epoch manager dependency from
consensus gossip only using the epoch manager events receiver.

Also directly encode and send the gossip message in the handle instead
of first routing the message through the consensus gossip service and
then to networking. The consensus gossip service is only responsible for
ensuring that the node is subscribed to the correct shard group topic.

Increased gossipsub message limit to 1MiB to account for large foreign
proposal messages which were rejected when testing. This is temporary
and improvements to the protocol should allow us to reduce this.

How Has This Been Tested?
---
Manually

What process can a PR reviewer use to test or verify this change?
---
Slight and certainly unobservable performance improvements, this is a
simplification PR

Breaking Changes
---

- [x] None
- [ ] Requires data directory to be deleted
- [ ] Other - Please specify

applications/tari_indexer/src/lib.rs
@@ -96,12 +96,7 @@ pub async fn run_indexer(config: ApplicationConfig, mut shutdown_signal: Shutdow
     )
     .await?;
 
-    let mut epoch_manager_events = services.epoch_manager.subscribe().await.map_err(|e| {
-        ExitError::new(
-            ExitCode::ConfigError,
-            format!("Epoch manager crashed on startup: {}", e),
-        )
-    })?;
+    let mut epoch_manager_events = services.epoch_manager.subscribe();
 
     let substate_cache_dir = config.common.base_path.join("substate_cache");
     let substate_cache = SubstateFileCache::new(substate_cache_dir)

applications/tari_validator_node/src/dan_node.rs
@@ -42,7 +42,7 @@ impl DanNode {
 
     pub async fn start(mut self, mut shutdown: ShutdownSignal) -> Result<(), anyhow::Error> {
         let mut hotstuff_events = self.services.consensus_handle.subscribe_to_hotstuff_events();
-        let mut epoch_manager_events = self.services.epoch_manager.subscribe().await?;
+        let mut epoch_manager_events = self.services.epoch_manager.subscribe();
 
         // if let Err(err) = self.dial_local_shard_peers().await {
         //     error!(target: LOG_TARGET, "Failed to dial local shard peers: {}", err);

applications/tari_validator_node/src/p2p/services/consensus_gossip/service.rs
@@ -20,28 +20,24 @@
 //  WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE
 //  USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-use std::fmt::Display;
-
 use libp2p::{gossipsub, PeerId};
 use log::*;
-use tari_consensus::messages::HotstuffMessage;
-use tari_dan_common_types::{Epoch, PeerAddress, ShardGroup};
+use tari_dan_common_types::ShardGroup;
 use tari_dan_p2p::{proto, TariMessagingSpec};
-use tari_epoch_manager::{base_layer::EpochManagerHandle, EpochManagerEvent, EpochManagerReader};
+use tari_epoch_manager::EpochManagerEvent;
 use tari_networking::{NetworkingHandle, NetworkingService};
 use tari_swarm::messaging::{prost::ProstCodec, Codec};
-use tokio::sync::{mpsc, oneshot};
+use tokio::sync::{broadcast, mpsc};
 
-use super::{ConsensusGossipError, ConsensusGossipRequest};
+use super::ConsensusGossipError;
 
 const LOG_TARGET: &str = "tari::validator_node::consensus_gossip::service";
 
 pub const TOPIC_PREFIX: &str = "consensus";
 
 #[derive(Debug)]
-pub(super) struct ConsensusGossipService<TAddr> {
-    requests: mpsc::Receiver<ConsensusGossipRequest>,
-    epoch_manager: EpochManagerHandle<TAddr>,
+pub(super) struct ConsensusGossipService {
+    epoch_manager_events: broadcast::Receiver<EpochManagerEvent>,
     is_subscribed: Option<ShardGroup>,
     networking: NetworkingHandle<TariMessagingSpec>,
     codec: ProstCodec<proto::consensus::HotStuffMessage>,
@@ -49,17 +45,15 @@ pub(super) struct ConsensusGossipService<TAddr> {
     tx_consensus_gossip: mpsc::Sender<(PeerId, proto::consensus::HotStuffMessage)>,
 }
 
-impl ConsensusGossipService<PeerAddress> {
+impl ConsensusGossipService {
     pub fn new(
-        requests: mpsc::Receiver<ConsensusGossipRequest>,
-        epoch_manager: EpochManagerHandle<PeerAddress>,
+        epoch_manager_events: broadcast::Receiver<EpochManagerEvent>,
         networking: NetworkingHandle<TariMessagingSpec>,
         rx_gossip: mpsc::UnboundedReceiver<(PeerId, gossipsub::Message)>,
         tx_consensus_gossip: mpsc::Sender<(PeerId, proto::consensus::HotStuffMessage)>,
     ) -> Self {
         Self {
-            requests,
-            epoch_manager,
+            epoch_manager_events,
             is_subscribed: None,
             networking,
             codec: ProstCodec::default(),
@@ -69,24 +63,16 @@ impl ConsensusGossipService<PeerAddress> {
     }
 
     pub async fn run(mut self) -> anyhow::Result<()> {
-        let mut events = self.epoch_manager.subscribe().await?;
-
         loop {
             tokio::select! {
-                Some(req) = self.requests.recv() => self.handle_request(req).await,
                 Some(msg) = self.rx_gossip.recv() => {
                     if let Err(err) = self.handle_incoming_gossip_message(msg).await {
                         warn!(target: LOG_TARGET, "Consensus gossip service error: {}", err);
                     }
                 },
-                Ok(event) = events.recv() => {
-                    if let EpochManagerEvent::EpochChanged(epoch) = event {
-                        if self.epoch_manager.is_this_validator_registered_for_epoch(epoch).await?{
-                            info!(target: LOG_TARGET, "Consensus gossip service subscribing messages for epoch {}", epoch);
-                            self.subscribe(epoch).await?;
-
-                            // TODO: unsubscribe older epoch shards?
-                        }
+                Ok(event) = self.epoch_manager_events.recv() => {
+                    if let EpochManagerEvent::ThisValidatorIsRegistered{shard_group, ..} = event {
+                        self.subscribe(shard_group).await?;
                     }
                 },
                 else => {
@@ -99,21 +85,6 @@ impl ConsensusGossipService<PeerAddress> {
         self.unsubscribe().await?;
 
         Ok(())
-    }
-
-    async fn handle_request(&mut self, request: ConsensusGossipRequest) {
-        match request {
-            ConsensusGossipRequest::Multicast {
-                shard_group,
-                message,
-                reply,
-            } => {
-                handle(reply, self.multicast(shard_group, message).await);
-            },
-            ConsensusGossipRequest::GetLocalShardGroup { reply } => {
-                handle(reply, self.get_local_shard_group().await);
-            },
-        }
     }
 
     async fn handle_incoming_gossip_message(
@@ -136,12 +107,10 @@ impl ConsensusGossipService<PeerAddress> {
         Ok(())
     }
 
-    async fn subscribe(&mut self, epoch: Epoch) -> Result<(), ConsensusGossipError> {
-        let committee_shard = self.epoch_manager.get_local_committee_info(epoch).await?;
-        let shard_group = committee_shard.shard_group();
-
+    async fn subscribe(&mut self, shard_group: ShardGroup) -> Result<(), ConsensusGossipError> {
         match self.is_subscribed {
             Some(sg) if sg == shard_group => {
+                debug!(target: LOG_TARGET, "🌬️ Consensus gossip already subscribed to messages for {shard_group}");
                 return Ok(());
             },
             Some(_) => {
@@ -150,9 +119,10 @@ impl ConsensusGossipService<PeerAddress> {
             None => {},
         }
 
+        info!(target: LOG_TARGET, "🌬️ Consensus gossip service subscribing messages for {shard_group}");
         let topic = shard_group_to_topic(shard_group);
         self.networking.subscribe_topic(topic).await?;
-        self.is_subscribed = Some(committee_shard.shard_group());
+        self.is_subscribed = Some(shard_group);
 
         Ok(())
     }
@@ -166,74 +136,13 @@ impl ConsensusGossipService<PeerAddress> {
 
         Ok(())
     }
-
-    pub async fn multicast(
-        &mut self,
-        shard_group: ShardGroup,
-        message: HotstuffMessage,
-    ) -> Result<(), ConsensusGossipError> {
-        // if we are alone in the local shard group, no need to broadcast
-        if self.num_shard_group_members().await? < 2 {
-            return Ok(());
-        }
-
-        let topic = shard_group_to_topic(shard_group);
-
-        debug!(
-            target: LOG_TARGET,
-            "multicast: topic: {}", topic,
-        );
-
-        let message = proto::consensus::HotStuffMessage::from(&message);
-        let mut buf = Vec::with_capacity(1024);
-        self.codec
-            .encode_to(&mut buf, message)
-            .await
-            .map_err(|e| ConsensusGossipError::InvalidMessage(e.into()))?;
-
-        self.networking.publish_gossip(topic, buf).await?;
-
-        Ok(())
-    }
-
-    async fn num_shard_group_members(&self) -> Result<u32, ConsensusGossipError> {
-        let epoch = self.epoch_manager.current_epoch().await?;
-
-        if self.epoch_manager.is_this_validator_registered_for_epoch(epoch).await? {
-            let committee_shard = self.epoch_manager.get_local_committee_info(epoch).await?;
-            return Ok(committee_shard.num_shard_group_members());
-        }
-
-        // default value if the VN is not registered
-        Ok(0)
-    }
-
-    pub async fn get_local_shard_group(&self) -> Result<Option<ShardGroup>, ConsensusGossipError> {
-        let epoch = self.epoch_manager.current_epoch().await?;
-
-        if !self.epoch_manager.is_this_validator_registered_for_epoch(epoch).await? {
-            return Ok(None);
-        }
-
-        let committee_shard = self.epoch_manager.get_local_committee_info(epoch).await?;
-        Ok(Some(committee_shard.shard_group()))
-    }
 }
 
-fn shard_group_to_topic(shard_group: ShardGroup) -> String {
+pub(super) fn shard_group_to_topic(shard_group: ShardGroup) -> String {
     format!(
         "{}-{}-{}",
         TOPIC_PREFIX,
         shard_group.start().as_u32(),
         shard_group.end().as_u32()
     )
-}
-
-fn handle<T, E: Display>(reply: oneshot::Sender<Result<T, E>>, result: Result<T, E>) {
-    if let Err(ref e) = result {
-        error!(target: LOG_TARGET, "Request failed with error: {}", e);
-    }
-    if reply.send(result).is_err() {
-        error!(target: LOG_TARGET, "Requester abandoned request");
-    }
 }

applications/tari_validator_node/src/p2p/services/mempool/service.rs
@@ -90,7 +90,7 @@ where TValidator: Validator<Transaction, Context = (), Error = TransactionValida
     }
 
     pub async fn run(mut self) -> anyhow::Result<()> {
-        let mut events = self.epoch_manager.subscribe().await?;
+        let mut events = self.epoch_manager.subscribe();
 
         loop {
             tokio::select! {

dan_layer/consensus/src/hotstuff/state_machine/idle.rs
@@ -33,7 +33,7 @@ where TSpec: ConsensusSpec
         context: &mut ConsensusWorkerContext<TSpec>,
     ) -> Result<ConsensusStateEvent, HotStuffError> {
         // Subscribe before checking if we're registered to eliminate the chance that we miss the epoch event
-        let mut epoch_events = context.epoch_manager.subscribe().await?;
+        let mut epoch_events = context.epoch_manager.subscribe();
         context.epoch_manager.wait_for_initial_scanning_to_complete().await?;
         let current_epoch = context.epoch_manager.current_epoch().await?;
         if self.is_registered_for_epoch(context, current_epoch).await? {

dan_layer/consensus/src/hotstuff/worker.rs
@@ -258,7 +258,7 @@ impl<TConsensusSpec: ConsensusSpec> HotstuffWorker<TConsensusSpec> {
         let mut on_force_beat = self.pacemaker.get_on_force_beat();
         let mut on_leader_timeout = self.pacemaker.get_on_leader_timeout();
 
-        let mut epoch_manager_events = self.epoch_manager.subscribe().await?;
+        let mut epoch_manager_events = self.epoch_manager.subscribe();
 
         let mut prev_height = self.pacemaker.current_view().get_height();
         let current_epoch = self.pacemaker.current_view().get_epoch();

dan_layer/epoch_manager/src/base_layer/base_layer_epoch_manager.rs
@@ -152,6 +152,7 @@ impl<TAddr: NodeAddressable + DerivableFromPublicKey>
         if let Some(vn) = vns.iter().find(|vn| vn.public_key == self.node_public_key) {
             self.publish_event(EpochManagerEvent::ThisValidatorIsRegistered {
                 epoch,
+                shard_group: vn.shard_key.to_shard_group(self.config.num_preshards, num_committees),
                 shard_key: vn.shard_key,
             });
         }

dan_layer/epoch_manager/src/event.rs
@@ -1,10 +1,14 @@
 //    Copyright 2023 The Tari Project
 //    SPDX-License-Identifier: BSD-3-Clause
 
-use tari_dan_common_types::{Epoch, SubstateAddress};
+use tari_dan_common_types::{Epoch, ShardGroup, SubstateAddress};
 
 #[derive(Debug, Clone)]
 pub enum EpochManagerEvent {
     EpochChanged(Epoch),
-    ThisValidatorIsRegistered { epoch: Epoch, shard_key: SubstateAddress },
+    ThisValidatorIsRegistered {
+        epoch: Epoch,
+        shard_group: ShardGroup,
+        shard_key: SubstateAddress,
+    },
 }

commit d1c842282be492060ab107a4fe0b0e6eae99055d

fix(gossip): bug vn might not be subscribed to consensus messages (#1191)
Description
---
fix(gossip): bug vn might not be subscribed to consensus messages
Removed unused `ThisValidatorIsRegistered` epoch manager event
Added `registered_shard_group` to `EpochChanged` event that is Some when
local VN is registered for the epoch

Motivation and Context
---
Bug introduced in #1190 where VNs might not be subscribed to consensus
messages.

How Has This Been Tested?
---
Manually, validator node that has been shutdown and restarted without
the epoch changing was not subscribed to consensus messages

What process can a PR reviewer use to test or verify this change?
---
As above

Breaking Changes
---

- [x] None
- [ ] Requires data directory to be deleted
- [ ] Other - Please specify

applications/tari_indexer/src/lib.rs
@@ -224,13 +224,12 @@ pub async fn run_indexer(config: ApplicationConfig, mut shutdown_signal: Shutdow
 }
 
 async fn handle_epoch_manager_event(services: &Services, event: EpochManagerEvent) -> Result<(), anyhow::Error> {
-    if let EpochManagerEvent::EpochChanged(epoch) = event {
-        let all_vns = services.epoch_manager.get_all_validator_nodes(epoch).await?;
-        services
-            .networking
-            .set_want_peers(all_vns.into_iter().map(|vn| vn.address.as_peer_id()))
-            .await?;
-    }
+    let EpochManagerEvent::EpochChanged { epoch, .. } = event;
+    let all_vns = services.epoch_manager.get_all_validator_nodes(epoch).await?;
+    services
+        .networking
+        .set_want_peers(all_vns.into_iter().map(|vn| vn.address.as_peer_id()))
+        .await?;
 
     Ok(())
 }

applications/tari_validator_node/src/dan_node.rs
@@ -75,13 +75,12 @@ impl DanNode {
     }
 
     async fn handle_epoch_manager_event(&mut self, event: EpochManagerEvent) -> Result<(), anyhow::Error> {
-        if let EpochManagerEvent::EpochChanged(epoch) = event {
-            let all_vns = self.services.epoch_manager.get_all_validator_nodes(epoch).await?;
-            self.services
-                .networking
-                .set_want_peers(all_vns.into_iter().map(|vn| vn.address.as_peer_id()))
-                .await?;
-        }
+        let EpochManagerEvent::EpochChanged { epoch, .. } = event;
+        let all_vns = self.services.epoch_manager.get_all_validator_nodes(epoch).await?;
+        self.services
+            .networking
+            .set_want_peers(all_vns.into_iter().map(|vn| vn.address.as_peer_id()))
+            .await?;
 
         Ok(())
     }

applications/tari_validator_node/src/p2p/services/consensus_gossip/service.rs
@@ -71,7 +71,8 @@ impl ConsensusGossipService {
                     }
                 },
                 Ok(event) = self.epoch_manager_events.recv() => {
-                    if let EpochManagerEvent::ThisValidatorIsRegistered{shard_group, ..} = event {
+                    let EpochManagerEvent::EpochChanged{ registered_shard_group, ..} = event ;
+                    if let Some(shard_group) = registered_shard_group{
                         self.subscribe(shard_group).await?;
                     }
                 },

applications/tari_validator_node/src/p2p/services/mempool/gossip.rs
@@ -87,10 +87,9 @@ impl MempoolGossip<PeerAddress> {
         }
     }
 
-    pub async fn subscribe(&mut self, epoch: Epoch) -> Result<(), MempoolError> {
-        let committee_shard = self.epoch_manager.get_local_committee_info(epoch).await?;
+    pub async fn subscribe(&mut self, shard_group: ShardGroup) -> Result<(), MempoolError> {
         match self.is_subscribed {
-            Some(b) if b == committee_shard.shard_group() => {
+            Some(b) if b == shard_group => {
                 return Ok(());
             },
             Some(_) => {
@@ -100,9 +99,9 @@ impl MempoolGossip<PeerAddress> {
         }
 
         self.networking
-            .subscribe_topic(shard_group_to_topic(committee_shard.shard_group()))
+            .subscribe_topic(shard_group_to_topic(shard_group))
             .await?;
-        self.is_subscribed = Some(committee_shard.shard_group());
+        self.is_subscribed = Some(shard_group);
         Ok(())
     }
 

applications/tari_validator_node/src/p2p/services/mempool/service.rs
@@ -101,11 +101,10 @@ where TValidator: Validator<Transaction, Context = (), Error = TransactionValida
                     }
                 }
                 Ok(event) = events.recv() => {
-                    if let EpochManagerEvent::EpochChanged(epoch) = event {
-                        if self.epoch_manager.is_this_validator_registered_for_epoch(epoch).await?{
-                            info!(target: LOG_TARGET, "Mempool service subscribing transaction messages for epoch {}", epoch);
-                            self.gossip.subscribe(epoch).await?;
-                        }
+                    let EpochManagerEvent::EpochChanged { epoch, registered_shard_group} = event;
+                    if let Some(shard_group) = registered_shard_group {
+                        info!(target: LOG_TARGET, "Mempool service subscribing transaction messages for {shard_group} in {epoch}");
+                        self.gossip.subscribe(shard_group).await?;
                     }
                 },
 

dan_layer/consensus/src/hotstuff/state_machine/idle.rs
@@ -45,7 +45,7 @@ where TSpec: ConsensusSpec
                 event = epoch_events.recv() => {
                     match event {
                         Ok(event) => {
-                            if let Some(event) = self.on_epoch_event(context, event).await? {
+                            if let Some(event) = self.on_epoch_event( event).await? {
                                 return Ok(event);
                             }
                         },
@@ -78,20 +78,18 @@ where TSpec: ConsensusSpec
         Ok(is_registered)
     }
 
-    async fn on_epoch_event(
-        &self,
-        context: &mut ConsensusWorkerContext<TSpec>,
-        event: EpochManagerEvent,
-    ) -> Result<Option<ConsensusStateEvent>, HotStuffError> {
+    async fn on_epoch_event(&self, event: EpochManagerEvent) -> Result<Option<ConsensusStateEvent>, HotStuffError> {
         match event {
-            EpochManagerEvent::EpochChanged(epoch) => {
-                if self.is_registered_for_epoch(context, epoch).await? {
+            EpochManagerEvent::EpochChanged {
+                epoch,
+                registered_shard_group,
+            } => {
+                if registered_shard_group.is_some() {
                     Ok(Some(ConsensusStateEvent::RegisteredForEpoch { epoch }))
                 } else {
                     Ok(None)
                 }
             },
-            EpochManagerEvent::ThisValidatorIsRegistered { .. } => Ok(None),
         }
     }
 }

dan_layer/consensus/src/hotstuff/worker.rs
@@ -534,8 +534,11 @@ impl<TConsensusSpec: ConsensusSpec> HotstuffWorker<TConsensusSpec> {
 
     async fn on_epoch_manager_event(&mut self, event: EpochManagerEvent) -> Result<(), HotStuffError> {
         match event {
-            EpochManagerEvent::EpochChanged(epoch) => {
-                if !self.epoch_manager.is_this_validator_registered_for_epoch(epoch).await? {
+            EpochManagerEvent::EpochChanged {
+                epoch,
+                registered_shard_group,
+            } => {
+                if registered_shard_group.is_none() {
                     info!(
                         target: LOG_TARGET,
                         "💤 This validator is not registered for epoch {}. Going to sleep.", epoch
@@ -554,7 +557,6 @@ impl<TConsensusSpec: ConsensusSpec> HotstuffWorker<TConsensusSpec> {
                 // If we can propose a block end, let's not wait for the block time to do it
                 // self.pacemaker.beat();
             },
-            EpochManagerEvent::ThisValidatorIsRegistered { .. } => {},
         }
 
         Ok(())

dan_layer/epoch_manager/src/base_layer/base_layer_epoch_manager.rs
@@ -139,7 +139,7 @@ impl<TAddr: NodeAddressable + DerivableFromPublicKey>
         let vns = validator_nodes.get_all_within_epoch(epoch, self.config.validator_node_sidechain_id.as_ref())?;
 
         let num_committees = calculate_num_committees(vns.len() as u64, self.config.committee_size);
-        for vn in &vns {
+        for vn in vns {
             validator_nodes.set_committee_shard(
                 vn.shard_key,
                 vn.shard_key.to_shard_group(self.config.num_preshards, num_committees),
@@ -149,13 +149,6 @@ impl<TAddr: NodeAddressable + DerivableFromPublicKey>
         }
 
         tx.commit()?;
-        if let Some(vn) = vns.iter().find(|vn| vn.public_key == self.node_public_key) {
-            self.publish_event(EpochManagerEvent::ThisValidatorIsRegistered {
-                epoch,
-                shard_group: vn.shard_key.to_shard_group(self.config.num_preshards, num_committees),
-                shard_key: vn.shard_key,
-            });
-        }
 
         Ok(())
     }
@@ -550,7 +543,16 @@ impl<TAddr: NodeAddressable + DerivableFromPublicKey>
             }
         }
 
-        self.publish_event(EpochManagerEvent::EpochChanged(self.current_epoch));
+        let num_committees = self.get_number_of_committees(self.current_epoch)?;
+        let shard_group = self
+            .get_our_validator_node(self.current_epoch)
+            .optional()?
+            .map(|vn| vn.shard_key.to_shard_group(self.config.num_preshards, num_committees));
+
+        self.publish_event(EpochManagerEvent::EpochChanged {
+            epoch: self.current_epoch,
+            registered_shard_group: shard_group,
+        });
 
         Ok(())
     }

dan_layer/epoch_manager/src/event.rs
@@ -1,14 +1,13 @@
 //    Copyright 2023 The Tari Project
 //    SPDX-License-Identifier: BSD-3-Clause
 
-use tari_dan_common_types::{Epoch, ShardGroup, SubstateAddress};
+use tari_dan_common_types::{Epoch, ShardGroup};
 
 #[derive(Debug, Clone)]
 pub enum EpochManagerEvent {
-    EpochChanged(Epoch),
-    ThisValidatorIsRegistered {
+    EpochChanged {
         epoch: Epoch,
-        shard_group: ShardGroup,
-        shard_key: SubstateAddress,
+        /// Some if the local validator is registered for the epoch, otherwise None
+        registered_shard_group: Option<ShardGroup>,
     },
 }

dan_layer/consensus_tests/src/support/epoch_manager.rs
@@ -37,7 +37,7 @@ impl TestEpochManager {
         }
     }
 
-    pub async fn set_current_epoch(&mut self, current_epoch: Epoch) -> &Self {
+    pub async fn set_current_epoch(&mut self, current_epoch: Epoch, shard_group: ShardGroup) -> &Self {
         self.current_epoch = current_epoch;
         {
             let mut lock = self.inner.lock().await;
@@ -45,9 +45,10 @@ impl TestEpochManager {
             lock.is_epoch_active = true;
         }
 
-        let _ = self
-            .tx_epoch_events
-            .send(EpochManagerEvent::EpochChanged(current_epoch));
+        let _ = self.tx_epoch_events.send(EpochManagerEvent::EpochChanged {
+            epoch: current_epoch,
+            registered_shard_group: Some(shard_group),
+        });
 
         self
     }

dan_layer/consensus_tests/src/support/harness.rs
@@ -289,7 +289,10 @@ impl Test {
         info!("🌟 Starting {epoch}");
         for validator in self.validators.values_mut() {
             // Fire off initial epoch change event so that the pacemaker starts
-            validator.epoch_manager.set_current_epoch(epoch).await;
+            validator
+                .epoch_manager
+                .set_current_epoch(epoch, validator.shard_group)
+                .await;
         }
 
         self.wait_for_all_validators_to_start_consensus().await;